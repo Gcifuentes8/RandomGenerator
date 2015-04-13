e9aed2fe493808271759710dea3dc8122f0549fd
class <class@CST_N904_14.cst.citytech.cuny.edu>
2015-04-13 14:30:02 -0400

    changed name

---
+++ src/gcifuentes8/RandomGenerator2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gcifuentes8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author class
+ */
+public class RandomGenerator2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO: Create a Scanner object to read keyboard input.
+         Scanner scanner = new Scanner(System.in);
+       
+         //TODO: Ask the user a lower and upper bound.
+         System.out.print("Enter a lower bound: ");
+         int lowerBound = scanner.nextInt();
+    }
+    
+}
